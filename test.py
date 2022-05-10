087498e305948693ee57f23b9766b63a509c4baf
Brian Roepke <broepke@gmail.com>
2022-05-10 16:28:35 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+import pytorch_lightning as pl
